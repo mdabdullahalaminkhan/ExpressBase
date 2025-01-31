47d86ee882cf150ea63f1c8c959793cb5513d255
MD ABDULLAH AL AMIN KHAN <mdabdullahalaminkhan@gmail.com>
2025-01-31 23:02:02 +0600

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,15 +2,16 @@ const express =require('express');
 const UsersController=require("../controllers/UsersController");
 const AuthVerifyMiddleware=require("../middlewares/AuthVerifyMiddleware");
 
-
 const router =express.Router();
-
 
 router.post("/registration",UsersController.registration);
 router.post("/login",UsersController.login);
 router.post("/profileUpdate",AuthVerifyMiddleware,UsersController.profileUpdate);
 router.get("/profileDetails",AuthVerifyMiddleware,UsersController.profileDetails);
 
+router.get("/RecoverVerifyEmail/:email",UsersController.RecoverVerifyEmail);
+router.get("/RecoverVerifyOTP/:email/:otp",UsersController.RecoverVerifyOTP);
+router.post("/RecoverResetPass",UsersController.RecoverResetPass);
 
 module.exports=router;
 
